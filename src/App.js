ecba207c2a72cbc49b8002d7284bb43801e8757f
Maja <maja_rucman@yahoo.co.uk>
2020-08-22 12:57:19 +0700

    app changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-roter-dom';
 
 function App() {
   return (
     <React.Fragment>
-	  
+	  <Router> 
+	    <Switch> 
+	      <Route exact path="/" component={Home} />
+	    </Switch>
+	  </Router>
 	</React.Fragment>
   );
 }
