ad7b7f8048eb9dbc5aad6b35b38bf91342aa7a5a
Maja <maja_rucman@yahoo.co.uk>
2020-08-22 13:05:17 +0700

    New pages

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
 	  <Router> 
 	    <Switch> 
 	      <Route exact path="/" component={Home} />
+	      <Route exact path="/about" component={About} />
+	      <Route exact path="/contact" component={Contact} />
+	      <Route exact component={NoMatch} />
 	    </Switch>
 	  </Router>
 	</React.Fragment>
