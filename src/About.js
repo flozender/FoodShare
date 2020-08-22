a8d450c0c063746f6c2c9ccc07f6ca4f8ee69fd5
Maja <maja_rucman@yahoo.co.uk>
2020-08-22 13:46:11 +0700

    Page content changes

---
+++ src/About.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const About = () => (
+	<div>
+	  <h2>Page Title</h2>
+	  <p>Text</p>
+	</div>
+)
---
+++ src/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Home = () => (
+	<div>
+	  <h2>Page Title</h2>
+	  <p>Text</p>
+	</div>
+)
