be782627c4fa22d85c846e884fbf091d3eede7ad
Asheer Rizvi <asheer.vil@gmail.com>
2020-08-22 13:10:05 +0530

    Changing the server port for dev.

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ require('./startup/config')();
 require('./startup/validation')();
 require('./startup/prod')(app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   log.info(`Listening on port ${port}`);
 });
