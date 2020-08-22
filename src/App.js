75b1776294974dd52f0df022049223896e85c480
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2020-08-22 13:57:37 +0530

    compartmentalize

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {Home} from './Home';
-import {About} from './About';
-import {Contact} from './Contact';
-import {NoMatch} from './NoMatch';
-import {Layout} from './components/Layout';
-import {Navigation} from './components/Navigation';
+import React from "react";
+// We need to import SVGs as react components, DM me when you see this -TH
+import logo from "./assets/logo.svg";
 
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home } from "./pages/home";
+import { About } from "./pages/about";
+import { Contact } from "./pages/contact";
+import { NoMatch } from "./pages/noMatch";
+import { Layout } from "./components/layout";
+import { Navigation } from "./components/navigation";
 
 function App() {
   return (
     <React.Fragment>
-	  <Navigation />
-	  <Layout>
-	  <Router> 
-	    <Switch> 
-	      <Route exact path="/" component={Home} />
-	      <Route exact path="/about" component={About} />
-	      <Route exact path="/contact" component={Contact} />
-	      <Route exact component={NoMatch} />
-	    </Switch>
-	  </Router>
-	  </Layout>
-	</React.Fragment>
+      <Navigation />
+      <Layout>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact component={NoMatch} />
+          </Switch>
+        </Router>
+      </Layout>
+    </React.Fragment>
   );
 }
 
